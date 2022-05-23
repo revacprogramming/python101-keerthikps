8eee8dddd5677f187fe6ca03df4d52ee0b845661
Keerthi P <93108967+keerthikps@users.noreply.github.com>
2022-05-23 07:03:45 +0000

    10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,28 +1,10 @@
 def myinput():
-    fname = input("Enter file name: ")
-    return fname
+  fname=input("Enter file:")
+  return fname
 def compute(fname):
   f=open(fname)
-  li=list()  
-  for line in f:
-    if line.startswith("From"): 
-      l=line.split() 
-      li.append(l)
-      for i in li:
-            print(i[1])
-  #print(li)      #for w in l:
-        #print(w[1])
-        #li.append(w)      
-  #return li       
-#def output(li): 
- # count=0
-  #for w in li:    
-   # count=count+1    
-    #print(w[1])
- # print("There were",count, "lines in the file with From as the first word")
-def main():
-  fname=input()
-  li=compute(fname)
-  #output(li)
-main()    
-
+  for line in f
+    if line.startswith("From")
+      mail=line.split()[1]
+        
+        
