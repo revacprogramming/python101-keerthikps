49ca46e950ccfca8850dac37bb4bf1587fe00100
Keerthi P <93108967+keerthikps@users.noreply.github.com>
2022-05-26 10:46:17 +0000

    coded

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -3,8 +3,15 @@ def myinput():
   return fname
 def compute(fname):
   f=open(fname)
+  count={}  
+  w2=dict()  
   for line in f
     if line.startswith("From")
-  
+      email=line.split()[1]
+      count[email]=count.get(email,0)+1
+  max=0
+  min=0
+  for email in count
+  if min>count[email]
         
         
