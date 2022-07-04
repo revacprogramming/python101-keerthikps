f01a66bcec3c6b51ccdff176cf7508abe17c671c
keerthikps <93108967+keerthikps@users.noreply.github.com>
2022-07-04 22:34:34 +0530

    Update problem#10.py

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,27 +1,29 @@
 def myinput():
-  fname=input("Enter file:")
-  return fname
-def compute(fname):
-  f=open(fname)
-  count={}  
-  w2=dict()  
-  for line in f
-    if line.startswith("From")
-      email=line.split()[1]
-      count[email]=count.get(email,0)+1
-  max_count=0
-  max_email=0
-  for email in count
-    if max_count<count[email]
-      max_count=count[email]
-      max_email=email
-  return max_count,max_email
-def output(max_count,max_email)
-  print(max_email,max_count)
-def main()
-  fname=myinput()
-  max_count,max_email=compute(fname)
-  output(max_count,max_count)
+  filename=input("Enter the file")
+  return filename
+def compute(filename):
+  count={}
+  filehandle=open(filename)
+  for line in filehandle:
+    words=line.split()
+    if line.startswith("From") and len(words)>3 :
+      w=words[1]
+      count[w]=count.get(w,0)+1  
+  maxword=0
+  maxcount=0
+  for w in count:
+    if maxcount<count[w]:
+      maxword=w
+      maxcount=count[w]
+  return maxword,maxcount
+def output(maxword,maxcount):
+  print(maxword,maxcount)
+def main():
+  filename=myinput()
+  maxword,maxcount=compute(filename)
+  output(maxword,maxcount)
 main()
+            
+      
         
         
