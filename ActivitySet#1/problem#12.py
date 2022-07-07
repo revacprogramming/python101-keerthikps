26c5c37e01e0e2b59f756882d44bf4dfd56db106
keerthikps <93108967+keerthikps@users.noreply.github.com>
2022-07-07 23:49:50 +0530

    Update problem#12.py

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -4,10 +4,10 @@ import re
 file=('dataset/mbox-short.txt')
 fhand=open(file)
 number=list()
-for line in fhand:
+for line in fhand
     line=line.rstrip()
     nums = re.findall('[0-9]+',line)
-    for i in nums:
+    for i in nums
         num=int(i)
         number.append(num)
     
