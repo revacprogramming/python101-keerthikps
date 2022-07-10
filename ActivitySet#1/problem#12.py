3fd7a5cd1e06731cf25658eb3da7181adf57bd4d
keerthikps <93108967+keerthikps@users.noreply.github.com>
2022-07-10 21:53:18 +0530

    Update problem#12.py

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,14 +1,14 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
-import r
-file=('dataset/mbox-short.txt')
+import re
+file=('dataset/mbox-short.txt'):
 fhand=open(file)
 number=list()
 for line in fhand:
     line=line.rstrip()
     nums = re.findall('[0-9]+',line)
-    for i in nums
+    for i in nums:
         num=int(i)
         number.append(num)
     
-print(sum(number))
+print(sum(num))
