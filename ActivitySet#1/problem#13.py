97ea2283fb5c9639c1909b449593bcc433963f1e
keerthikps <93108967+keerthikps@users.noreply.github.com>
2022-07-20 23:07:59 +0530

    Update problem#13.py

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,7 +1,7 @@
-# Network Programming
+
 # https://www.py4e.com/lessons/network
 import socket
-import time
+import tim
 
 HOST = 'data.pr4e.org' #linking another website
 PORT = 80 #for http constant
