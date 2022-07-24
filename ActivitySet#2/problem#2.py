a8f18417fd2c6c6a4ac22024d356326b36c38e69
keerthikps <93108967+keerthikps@users.noreply.github.com>
2022-07-24 21:20:29 +0530

    Update problem#2.py

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -6,7 +6,11 @@ def add(a, b):
 def output(a, b, sum):
     pass  # ...
 
-
+def input_two_numbers():
+  a=float(input("Enter a number: "))
+  b=float(input("Enter a number: "))
+  return a,b
+  
 def main():
     a, b = input_two_numbers()
     sum = add(a, b)
@@ -14,5 +18,6 @@ def main():
     output(a, b, sum)
 
 
-if __name__ == '__main__':
+_name_=input("Enter the name: ")
+if _name_ == '_main_':
     main()
