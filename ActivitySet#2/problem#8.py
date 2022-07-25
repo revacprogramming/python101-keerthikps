d72dac03591493a51ca02992d4f0fe5d2db599e3
keerthikps <93108967+keerthikps@users.noreply.github.com>
2022-07-25 23:48:06 +0530

    Update problem#8.py

--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -1,11 +1,18 @@
+class Menu(dict):
+  def __init__(self):
+   pass
+  def dict(self,item,rate):
+    self[item]=rate
+    
+menu=Menu()
+menu['Idly']=20
+menu['Dosa']=25
+menu['Vada']=10
 
-
-class Menu:
-    """fill in class definition here"""
-
-
-m = Menu()
-m["idly"] = 10
-m["vada"] = 20
-
-print(m)
+print(menu)
+for k,v in menu.items():
+  print(k,v)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
