882ab736d2710b37880544ced82afba74651fb20
keerthikps <93108967+keerthikps@users.noreply.github.com>
2022-07-25 23:44:23 +0530

    Update problem#14.py

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,14 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+import xml.etree.Elementtree as ET
+data = '''
+<person>
+<name>ABCD</name>
+<phone type = " intl ">
+</phone>
+<email hide = "yes">
+</person> '''
+
+tree = ET.fromstring(data)
+print('Name:', tree.find('name').text)
+print('Attr:', tree.find('email').get('hide'))
