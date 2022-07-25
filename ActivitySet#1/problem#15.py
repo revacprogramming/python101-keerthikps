582346280daaf8ba22dd2d1fcb53cf040312c341
keerthikps <93108967+keerthikps@users.noreply.github.com>
2022-07-25 23:45:08 +0530

    Update problem#15.py

--- ActivitySet#1/problem#15.py
+++ ActivitySet#1/problem#15.py
@@ -1,2 +1,18 @@
 # Object Oriented Programming
 # https://www.py4e.com/lessons/Objects
+import json
+data = '''
+{
+"name" = "ABCD",
+"phone" = {
+    "type"="intl"
+    "number" = "1234567890"
+    },
+ "email":{
+"hide"="yes"
+ }
+}'''
+
+info = json.loads(data)
+print('Name:',info["name"])
+print('Hide:',info["email"]["hide"]) 
