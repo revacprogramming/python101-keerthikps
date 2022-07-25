3afc4e5a238fb2a82aa2b50037ada4e70c620d33
keerthikps <93108967+keerthikps@users.noreply.github.com>
2022-07-25 23:46:01 +0530

    Update problem#16.py

--- ActivitySet#1/problem#16.py
+++ ActivitySet#1/problem#16.py
@@ -1,2 +1,11 @@
 # Databases
 # https://www.py4e.com/lessons/database
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+counts = dict()
+for line in fhand:
+  words = line.decode().split()
+  for word in words:
+    counts[word] = counts.get(word, 0) + 1
+print(counts) 
